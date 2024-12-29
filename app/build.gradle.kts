e4dc90b0c0dd9ec53779bfc97d3645a034539555
myself-sohan <sohansaha.11sc.2020@gmail.com>
2024-12-30 01:56:53 +0530

    Icon Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     //Dagger - Hilt
     implementation ("com.google.dagger:hilt-android:2.51.1")
-    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    //implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     kapt("androidx.hilt:hilt-compiler:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
